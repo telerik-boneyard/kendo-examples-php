9b7264fddfee77493b523e5dfd81527fe1a80244
Hélder Pereira <spiider@users.noreply.github.com>
2014-03-05 14:42:50 +0000

    Update employees.php

--- northwind/data/employees.php
+++ northwind/data/employees.php
@@ -5,12 +5,8 @@ mysql_select_db("northwind") or die("Unable To Connect To Northwind");
 $arr = array();
 $rs = mysql_query("SELECT EmployeeID, LastName, FirstName FROM Employees");
  
-while($obj = mysql_fetch_object($rs)) {
-	$arr[] = $obj;
+while($obj[] = mysql_fetch_object($rs)) {
+	$arr["data"] = $obj;
 }
-
-// add the header line to specify that the content type is JSON
 header("Content-type: application/json"); 
-
-echo "{\"data\":" .json_encode($arr). "}";
-?>
+echo json_encode($arr);
